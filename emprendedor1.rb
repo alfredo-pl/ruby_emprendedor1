45efc41c522c8302e08781c1297dd1dcc9c37e74
alfredopl <aalfredoplz@gmail.com>
2020-10-14 12:55:11 -0300

    Changing the outputs

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -1,15 +1,16 @@
-puts"ingrese el precio"
-pr = ARGV[0].to_i
-puts"ingrese el numero de usuarios"
+
+pr = ARGV[0].to_f
 nu = ARGV[1].to_i
-puts"ingrese los gastos"
-g = ARGV[2].to_i
+g = ARGV[2].to_f
+puts"El precio que ingreso es: #{pr}"
+puts"El numero de usuarios es: #{nu}"
+puts"Los gastos son: #{g}"
 
 u= pr * nu - g
 if u > 0 
     imp = u *0.35
     u = u - imp
-    puts"los impuestos aplicados son #{imp} y su utilidad es de #{u}"
+    puts"los impuestos aplicados son #{imp.to_i} y su utilidad es de #{u.to_i}"
 else 
     puts"no se aplicaron impuestos y su utilidad es de #{u} ,no es rentable"
 end
